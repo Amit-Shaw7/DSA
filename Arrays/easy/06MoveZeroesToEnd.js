ef7cd79cb0c200b6b2465d1588ca2c2151ffdab2
Amit-Shaw7 <maahiamit777@gmail.com>
2025-05-24 00:56:58 +0530

    added move zero to end optimal approach and linear search algo

--- Arrays/easy/06MoveZeroesToEnd.js
+++ Arrays/easy/06MoveZeroesToEnd.js
@@ -2,14 +2,14 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function (nums) {
+var moveZeroesBrute = function (nums) {
   let n = nums.length;
 
   for (let i = 0; i < n; i++) {
     if (nums[i] === 0) {
       let idx = i;
 
-      while (idx < n -1) {
+      while (idx < n - 1) {
         let temp = nums[idx];
         nums[idx] = nums[idx + 1];
         nums[idx + 1] = temp;
@@ -21,7 +21,29 @@ var moveZeroes = function (nums) {
   }
 };
 
-const nums = [0,0,1]
-// const nums = [1,3,12,0,0]
-moveZeroes(nums);
+var moveZeroesOptimal = function (nums) {
+  let n = nums.length;
+  let indexOfZero = nums?.findIndex((num) => num === 0);
+
+  console.log("zero  index : " + indexOfZero);
+
+  if (indexOfZero < 0 || nums.length === 1) return;
+
+  let indexOfNextToZero = indexOfZero + 1;
+
+  for (let i = indexOfNextToZero; i < n; i++) {
+    if (nums[indexOfNextToZero] !== 0) {
+      let temp = nums[indexOfNextToZero];
+      nums[indexOfNextToZero] = nums[indexOfZero];
+      nums[indexOfZero] = temp;
+      indexOfZero++;
+    }
+    indexOfNextToZero++;
+  }
+};
+
+const nums = [0, 0, 1];
+// let nums = [0, 12, 0, 0, 0, 0, 0, 12];
+
+moveZeroesOptimal(nums);
 console.log(nums);
---
+++ Arrays/easy/07LinearSearch.js
@@ -0,0 +1,19 @@
+let arr = [5, 6, 4, 2, 3, 5, 66, 90];
+let target = 66;
+
+const searchLinearly = () => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+
+  return -1;
+};
+
+const idx = searchLinearly(arr, target);
+if (idx < 0) {
+  console.log("Element not found");
+} else {
+  console.log("Element found at index : ", idx);
+}
